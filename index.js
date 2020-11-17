f35a671a6a71094ddc997bbd2106193c558b34d2
emmanielsen <emma6007@edu.ucl.dk>
2020-11-17 13:03:25 +0100

    menu

--- index.js
+++ index.js
@@ -19,32 +19,16 @@ function carousel() {
 /* hvad betyder $*/ 
 
 
-$('.menu').click(function(){
-  $(this).toggleClass('icon-menu');
-  $(this).toggleClass('icon-cross');
-  $('nav').toggleClass('down');
-  $('nav li a').removeClass('down');
-  $('.search').removeClass('down');
-  $('.icon-search').removeClass('icon-cross');
-});
-
-//Make sure the menu icon behaves corectly when the menu is open
-$('nav li a').click(function(){
-    $('.menu').addClass('icon-menu');
-    $('.menu').removeClass('icon-cross');
-    $('nav').toggleClass('down');
-});
-
-//show and hide the search bar, also make sure if the menu is open to hide the menu, and ensure the menu icon state is correct
-$('.icon-search').click(function(){
-    $(this).toggleClass('icon-cross');
-    $('.menu').addClass('icon-menu');
-    $('.menu').removeClass('icon-cross');
-    $('.search').toggleClass('down');
-    $('nav').removeClass('down');
-});
-
-
+function toggleCP(){
+	var cp = document.getElementById("cp");
+	if(cp.style.opacity == 1){
+		cp.style.opacity = 0;
+		cp.style.right = "-260px"; // remove it from active screen space
+	} else {
+		cp.style.right = "0px"; // return it to active screen space
+		cp.style.opacity = 1;
+	}
+}
 
 /* slider */ 
 
