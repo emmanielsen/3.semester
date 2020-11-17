69815d01909d313a7bdd9f74e1a562652976b126
emmanielsen <emma6007@edu.ucl.dk>
2020-11-17 09:46:21 +0100

    menu

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+console.log('emma');
 /* slideshow galleri */ 
 var myIndex = 0;
 carousel();
@@ -17,12 +18,30 @@ function carousel() {
 /* buger menu*/ 
 /* hvad betyder $*/ 
 
-("#menu-button").click(function(){
-  (this).toggleClass("active");
-  ("#line-1").toggleClass("active");
-  ("#line-2").toggleClass("active");
-  ("#line-3").toggleClass("active");
-  ("#menu").slideToggle("slow");
+
+$('.menu').click(function(){
+  $(this).toggleClass('icon-menu');
+  $(this).toggleClass('icon-cross');
+  $('nav').toggleClass('down');
+  $('nav li a').removeClass('down');
+  $('.search').removeClass('down');
+  $('.icon-search').removeClass('icon-cross');
+});
+
+//Make sure the menu icon behaves corectly when the menu is open
+$('nav li a').click(function(){
+    $('.menu').addClass('icon-menu');
+    $('.menu').removeClass('icon-cross');
+    $('nav').toggleClass('down');
+});
+
+//show and hide the search bar, also make sure if the menu is open to hide the menu, and ensure the menu icon state is correct
+$('.icon-search').click(function(){
+    $(this).toggleClass('icon-cross');
+    $('.menu').addClass('icon-menu');
+    $('.menu').removeClass('icon-cross');
+    $('.search').toggleClass('down');
+    $('nav').removeClass('down');
 });
 
 
