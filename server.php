f362d6c0e02f0818b57a1a3426f76432cc82a713
emmanielsen <emma6007@edu.ucl.dk>
2020-11-18 12:46:09 +0100

    booking system når det er bedst

--- server.php
+++ server.php
@@ -20,46 +20,6 @@ if (isset($_POST['submit'])) {
   } else {
       $_SESSION['error'] = "Undskyld der er sket en fejl"; 
   }
-
-// authentication
-   if($password1 !== $password2) {
-      array_push($errors, 'Passwords do not match!');
-      header('location: demo.php');
-      exit();
-   }
-
-//    VALIDATION
-   $check_query = "SELECT email FROM users WHERE email = '$email' "; 
-   $returns = mysqli_query($db, $check_query);
-      if (mysqli_num_rows($returns) > 0) {
-          array_push($errors, 'This User Email already exists');
-              header('location: demo.php');
-              exit();
-      } else {
-          $query = "INSERT INTO users(username, email, password) VALUES ('$username', '$email', '$password1') ";
-          $return = mysqli_query($db, $query);
-          if ($return) {
-              $new_user = mysqli_fetch_all($return, MYSQLI_ASSOC);
-              $_SESSION['new_id'] = $db->$insert_db;
-          }
-
-        }
-
-        $query = "SELECT * FROM users WHERE id = " . $_SESSION['new_id'];
-        $return_result = mysqli_query($db, $query);
-
-        if ($return_result) {
-            if ($mysqli_num_rows($return_result) > 0) {
-                $new_user = mysqli_fetch_all($return_result, MYSQLI_ASSOC);
-                $_SESSION['username'] = $new_user['username'];
-                $_SESSION['email'] = $new_user['email'];
-                $_SESSION['success'] = "Great!! You are now a member here! Your Email is: " . $_SESSION['email'];
-                header('location: index.php');
-                exit();
-            }
-        }
-
-   
 }
 
  ?>
